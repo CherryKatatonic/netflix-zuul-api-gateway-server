ee11bddbe9d19dfe3bc2beabafdabdb1f1401f61
nunya biz <nunya@bizniz.con>
2020-09-26 19:04:01 -0400

    Add info log to ZuulLoggingFilter

--- src/main/java/com/katgrennan/currencyexchange/netflixzuulapigatewayserver/ZuulLoggingFilter.java
+++ src/main/java/com/katgrennan/currencyexchange/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -1,11 +1,19 @@
 package com.katgrennan.currencyexchange.netflixzuulapigatewayserver;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class ZuulLoggingFilter extends ZuulFilter {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public String filterType() {
         return "pre";
@@ -23,6 +31,8 @@ public class ZuulLoggingFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
+        HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
+        logger.info("request -> {} request uri -> {}", req, req.getRequestURI());
         return null;
     }
 }
