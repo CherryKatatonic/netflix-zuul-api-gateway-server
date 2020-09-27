b12f802b8b08712692810a103e66a5685d61e923
nunya biz <nunya@bizniz.con>
2020-09-26 20:35:57 -0400

    Add Sampler to catch all logs

--- src/main/java/com/katgrennan/currencyexchange/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/com/katgrennan/currencyexchange/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -1,9 +1,11 @@
 package com.katgrennan.currencyexchange.netflixzuulapigatewayserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 // Enable Zuul API Gateway
 @EnableZuulProxy
@@ -14,6 +16,11 @@ public class NetflixZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
